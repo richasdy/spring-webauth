707f86634ace0722d56b298983463ae3903908b3
Donni Richasdy <donni.richasdy@gmail.com>
2017-03-28 17:13:06 +0700

    adding multiple user

--- src/main/java/com/richasdy/springwebauth/WebSecurityConfig.java
+++ src/main/java/com/richasdy/springwebauth/WebSecurityConfig.java
@@ -1,38 +1,38 @@
 package com.richasdy.springwebauth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-            	.antMatchers("/").permitAll()
-            	.antMatchers("/register").permitAll()
-                .antMatchers("/plugins/**").permitAll()
-                .antMatchers("/bootstrap/**").permitAll()
-                .antMatchers("/dist/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
-    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/register").permitAll()
+				.antMatchers("/plugins/**").permitAll().antMatchers("/bootstrap/**").permitAll().antMatchers("/dist/**")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and()
+				.logout().permitAll();
+	}
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER");
-    }
+	// @Autowired
+	// public void configureGlobal(AuthenticationManagerBuilder auth) throws
+	// Exception {
+	// auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	// }
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+		manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+		manager.createUser(User.withUsername("admin").password("password").roles("USER", "ADMIN").build());
+		return manager;
+	}
 }
